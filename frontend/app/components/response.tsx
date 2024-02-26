7865e2d9a7e4917d6c3e177284765b65238530eb
Harsh kumar <harshkumarsingh005@gmail.com>
2024-02-26 14:45:19 +0530

    added acertinity ui

---
+++ frontend/app/components/response.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from "next/image"
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { BackgroundBeams } from './ui/background-beams';
+
+const Response =()=>{
+    return(
+        <>
+        <BackgroundBeams/>
+        </>
+
+    )
+}
+export default Response
